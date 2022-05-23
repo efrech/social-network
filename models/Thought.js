1aed08e2f4c58a463a8abd46cbab1db506cb1819
Evelyn Frech <evelynmariafrech@gmail.com>
2022-05-22 21:44:40 -0400

    create models Schema and associations

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+const { timeStamp } = require('console');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Please leave your thought',
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+            unique: true,
+            get: timeStamp => dateFormat(timeStamp)
+        },
+        userNAme: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        // Virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+            getters: true,
+          },
+          id: false,
+    }
+);
+
+// Create a virtual property `reactionCount` that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// Initialize our User model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        // Virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+            virtuals: true,
+          },
+          id: false,
+    }
+);
+
+// Create a virtual property `friendCount` that retrieves the length of the friends array field on query.
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
